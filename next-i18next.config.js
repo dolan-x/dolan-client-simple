6efa32a0bb9635bd0c5ea5772e18678c1c483571
so1ve <nn_201312@163.com>
2021-09-25 20:50:23 +0800

    fix: 404 on vercel

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,10 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path')
+
 module.exports = {
   i18n: {
     defaultLocale: 'zh-Hans',
     locales: ['zh-Hans', 'en']
-  }
+  },
+  localePath: path.resolve('./public/locales')
 }
